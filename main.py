3e360d1d052a044240fe9c94f1535043a41fa9e4
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-08-07 22:03:25 +0530

    main.py file added here

---
+++ main.py
@@ -0,0 +1,25 @@
+from plyer import notification
+import time
+import tqdm
+
+
+#in this project you are asked how many time you need to remind and for houw much hour . 
+#that is very simple project and complete it as fast as possible . 
+
+def reminder():
+    count=0
+    while True:
+        notification.notify(title = 'Health',message = 'Drink water',app_icon = None,timeout = 10)
+        count+=1
+        for i in tqdm.tqdm(range(int(60))):
+            time.sleep(1)
+        if count>4:
+            break
+        else:
+            pass
+
+
+
+
+if __name__=="__main__":
+    reminder()
